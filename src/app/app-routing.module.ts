008a0e144eab5a8a59b106ca17f56783ea1eae7e
Claudia Ximena Bustamante Gómez - Ceiba Software <claudia.bustamante@ceiba.com.co>
2020-03-10 13:39:34 -0500

    User List created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
+import { UserComponent } from './user/user.component';
 import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/appComponent', pathMatch: 'full' },
-  { path: 'appComponent', component: AppComponent }
+  { path: '', redirectTo: '/UserComponent', pathMatch: 'full' },
+  { path: 'appComponent', component: AppComponent },
+  { path: 'userComponent', component: UserComponent }
 ];
 
 @NgModule({
